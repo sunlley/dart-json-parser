aa2f4e68a4804ff3fe0334ca5ab064ef116e0057
GitHub Action <action@github.com>
2023-06-20 06:39:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/sunlley/dartjsonparser/MyBundle.kt
+++ src/main/kotlin/com/github/sunlley/dartjsonparser/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.sunlley.dartjsonparser
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/sunlley/dartjsonparser/services/MyProjectService.kt
+++ src/main/kotlin/com/github/sunlley/dartjsonparser/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.sunlley.dartjsonparser.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sunlley.dartjsonparser.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
